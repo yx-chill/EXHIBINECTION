8f06d528725a3f19bf86520705b4af935b5b0bdc
Hua_Nan <x565619@gmail.com>
2021-08-11 16:56:42 +0800

    Update 2021-08-11T08:56:35.830Z

--- app/assets/js/all.js
+++
@@ -1,4 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 0,
-});
---
+++ assets/js/all.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 0
+});
+//# sourceMappingURL=all.js.map
